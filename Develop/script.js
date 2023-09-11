18ed84b240faad5e8e6e15b6359764f67ffe3213
Camsca <camila_vass@hotmail.com>
2023-09-10 21:32:33 -0700

    actual day on header

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
 $(".saveBtn").on("click",function(){
   var blockId = $(this).closest (".time-block").attr("id");
@@ -28,7 +26,13 @@ $(function(){
       $(this).addClass ('past');
     } else if (blockHour === currentHour){
       $(this).addClass ('present');
+    }else {
+      $(this).addClass('future');
     }
   
 
   });
+});
+ var currentDate = daysjs().format('MMMM D , YYYY');
+ $('#currentDay').text(currentDate);
+ 
