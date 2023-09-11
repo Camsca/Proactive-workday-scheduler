994bbca1cb1e33e09dc5eb1d56a7d3a15d7615ef
Camsca <camila_vass@hotmail.com>
2023-09-10 21:41:39 -0700

    correcting daysjs to dayjs

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,26 @@
 
 $(function () {
-$(".saveBtn").on("click",function(){
-  var blockId = $(this).closest (".time-block").attr("id");
-   console.log("Block ID:", blockId); 
-  var description = $(this).siblings(".description").val();
-  localStorage.setItem(blockId, description);
-});
-$(".time-block").each(function () {
-  var blockId = $(this).attr("id");
-  var description = localStorage.getItem(blockId);
-  if (description !== null) {
-    $(this).find(".description").val(description);
-  }
-});
-
+  $(".saveBtn").on("click",function(){
+    var blockId = $(this).closest (".time-block").attr("id");
+    console.log("Block ID:", blockId); 
+    var description = $(this).siblings(".description").val();
+    localStorage.setItem(blockId, description);
+  });
+  $(".time-block").each(function () {
+    var blockId = $(this).attr("id");
+    var description = localStorage.getItem(blockId);
+    if (description !== null) {
+      $(this).find(".description").val(description);
+    }
+  });
+  
 });
 
 $(function(){
   //get actual hour in a 24hr format
   var currentHour = dayjs().format('H');
   
+  //iterate throuhgh the time blocks and aplly the appropriate classes
   $('.time-block').each(function(){
     var blockHour = parseInt($(this).attr('id').split('-')[1]);
     if (blockHour<currentHour){
@@ -29,10 +30,12 @@ $(function(){
     }else {
       $(this).addClass('future');
     }
-  
-
+    
+    
   });
 });
- var currentDate = daysjs().format('MMMM D , YYYY');
+
+// Get the current date and display it in the header
+ var currentDate = dayjs().format('MMMM D , YYYY');
  $('#currentDay').text(currentDate);
- 
+//
