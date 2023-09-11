a5bd3ba66550444d3cf31c6a74fe0d6d5ca586ba
Camsca <camila_vass@hotmail.com>
2023-09-11 13:53:29 -0700

    checking why colors dont pop in set hour

--- Develop/script.js
+++ Develop/script.js
@@ -5,12 +5,15 @@ $(function () {
     var blockId = $(this).closest (".time-block").attr("id");
     console.log("Block ID:", blockId); 
     var description = $(this).siblings(".description").val();
+    console.log("Description:", description); 
     localStorage.setItem(blockId, description);
   });
    // On page load, retrieve and populate the descriptions from local storage
   $(".time-block").each(function () {
     var blockId = $(this).attr("id");
+    console.log("Loading from Local Storage - Block ID:", blockId);
     var description = localStorage.getItem(blockId);
+    console.log("Loaded Description:", description);
     if (description !== null) {
       $(this).find(".description").val(description);
     }
@@ -21,10 +24,11 @@ $(function () {
 $(function(){
   //get actual hour in a 24hr format
   var currentHour = dayjs().format('H');
-  
+  console.log("Current Hour:", currentHour);
   //iterate throuhgh the time blocks and aply the appropriate classes
   $('.time-block').each(function(){
     var blockHour = parseInt($(this).attr('id').split('-')[1]);
+    console.log("Block Hour:", blockHour);
     if (blockHour<currentHour){
       $(this).addClass ('past');
     } else if (blockHour === currentHour){
@@ -39,4 +43,5 @@ $(function(){
 
 // Get the current date and display it in the header
  var currentDate = dayjs().format('MMMM D , YYYY');
+ console.log("Current Date:", currentDate); 
  $('#currentDay').text(currentDate);
